b387e95a6dc150cf92292c59582d1e533caca5db
sks-7 <sachinsinghofficial7@gmail.com>
2023-03-02 14:11:49 +0530

    SetUp Is Completed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Hello Sachin Kumar</h3>
+    </div>
+  )
+}
+
+export default App
